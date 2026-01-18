a79e93c30f489bf4a770cdfbd11dbd7129bb8d20
tushar-41 <tusharkmr4126@gmail.com>
2026-01-18 22:36:07 +0530

    added database and prisma

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,24 +2,17 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { SigninSchema, UserSchema } from "@repo/common/types";
+import { UserSchema } from "@repo/common/types";
+// import { prismaClient } from "@repo/db/client";
 
 const app = express();
-
-app.listen(7676, () => {
-  console.log("HTTP backend server is running on http://localhost:7676");
-});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is live");
 });
 
 app.post("/signin", (req, res) => {
-  const { name, password } = req.body;
-
-  if (!name) return new Error("Name is not present");
-  if (!password) return new Error("Password is not present");
-
   let userId = 1;
   const token = jwt.sign(
     {
@@ -37,16 +30,34 @@ app.post("/signin", (req, res) => {
   });
 });
 
-app.post("/signup", (req, res) => {
-  const data = UserSchema.safeParse(req.body);
-  if (!data.success) {
+app.post("/signup", async (req, res) => {
+  const parsedData = UserSchema.safeParse(req.body);
+  if (!parsedData.success) {
     res.json({
       message: "Incorrect inputs",
     });
     return;
   }
-  //Sign up information
-  //db call
+
+  // try {
+  //   const user = await prismaClient.user.create({
+  //     data: {
+  //       email: parsedData.data.email,
+  //       name: parsedData.data.name,
+  //       password: parsedData.data.password,
+  //     },
+  //   });
+
+  //   res.json({
+  //     userId: user.id,
+  //   });
+  // } catch (error) {
+  //   res.status(411).json({
+  //     error: error,
+  //     message:
+  //       "This email address already registered please us another email adress",
+  //   });
+  // }
 });
 
 app.post("room", middleware, (req, res) => {
@@ -55,4 +66,8 @@ app.post("room", middleware, (req, res) => {
   res.json({
     roomId: Math.random(),
   });
+});
+
+app.listen(7676, () => {
+  console.log("HTTP backend server is running on http://localhost:7676");
 });
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 
 export const UserSchema = z.object({
   name: z.string(),
+  email: z.string(),
   password: z.string().min(5).max(15),
   username: z.string().min(3).max(20),
+  photo: z.optional(z.string()),
 });
 
 export const SigninSchema = z.object({
