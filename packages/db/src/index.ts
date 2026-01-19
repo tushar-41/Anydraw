fe4efc8cab66bc4d8f1c26234a61edda7541c7e3
tushar-41 <tusharkmr4126@gmail.com>
2026-01-20 01:28:10 +0530

    .env issue

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,15 +1,12 @@
-import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString =
-  process.env.DATABASE_URL ||
-  "postgresql://neondb_owner:npg_RW7EAFuqN5dK@ep-holy-tree-ah7cm87z-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
-
-if (!connectionString) throw new Error("DATABASE_URL missing");
+const connectionString = `${process.env.DATABASE_URL}`;
+if (!connectionString) {
+  throw new Error("Database URL missing"); //fix this later
+}
 
 const adapter = new PrismaPg({ connectionString });
+const prismaClient = new PrismaClient({ adapter });
 
-export const prismaClient = new PrismaClient({ adapter });
-
-console.log(process.env.DATABASE_URL);
+export { prismaClient };
